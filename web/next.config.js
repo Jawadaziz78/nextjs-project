2d79c79fb518064c991aa739a665547d561937ac
Ubuntu <ubuntu@ip-172-31-77-148.ec2.internal>
2025-12-15 17:36:05 +0000

    Updated next.config.js for dynamic base path

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
-}
-module.exports = nextConfig
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
